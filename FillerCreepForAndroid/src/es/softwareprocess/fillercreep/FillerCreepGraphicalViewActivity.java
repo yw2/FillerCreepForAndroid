32f7093802dcf6ae1719972c7193b2b0afceba16
yw2 <yw2@ualberta.ca>
2014-01-08 18:18:24 -0700

    Done

---
+++ FillerCreepForAndroid/src/es/softwareprocess/fillercreep/FillerCreepGraphicalViewActivity.java
@@ -0,0 +1,49 @@
+package es.softwareprocess.fillercreep;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class FillerCreepGraphicalViewActivity extends Activity implements
+        FView<FillerCreep> {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graphicalview);
+        ImageButton button = (ImageButton) findViewById(R.id.maingraphicalview);
+        OnClickListener listener = new OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        };
+        button.setOnClickListener(listener);
+
+        FillerCreep fc = FillerCreepApplication.getFillerCreep();
+        fc.addView(this);
+
+        updateMap();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        FillerCreep fc = FillerCreepApplication.getFillerCreep();
+        fc.deleteView(this);
+    }
+
+    public void update(FillerCreep fillerCreep) {
+        updateMap();
+    }
+
+    public void updateMap() {
+        ImageButton button = (ImageButton) findViewById(R.id.maingraphicalview);
+        GameController gc = FillerCreepApplication.getGameController();
+        Bitmap bitmap = gc.getMapBitmap();
+        button.setImageBitmap(bitmap);
+
+    }
+}
